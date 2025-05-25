44d917a44afc822af9fa2bb48ee2424a2401beb0
Jav1ex <nejaroes@gmail.com>
2025-05-25 00:49:09 -0500

    ms personal

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ Dependencies: SQLAlchemy.
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres.olzrkkjzqltovkvxeggb:WbnS5utrc1Gayg35@aws-0-us-east-2.pooler.supabase.com:5432/postgres"
+DATABASE_URL = "postgresql://postgres.olzrkkjzqltovkvxeggb:yVBwxlW1lAdlG9rp@aws-0-us-east-2.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
